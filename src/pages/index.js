775c4617ab25c588f609a077887cf55c789b5441
Matthew Savelev <savelevmatthew@gmail.com>
2022-09-05 00:56:42 +0500

    ci: eslint added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
+import Layout from '../components/layout'
 
-const IndexPage = props => {
-  return <Layout>Hey guys hope you are ready to learn!</Layout>
+const IndexPage = () => {
+    return <Layout>Hey guys hope you are ready to learn!</Layout>
 }
 
 export default IndexPage
